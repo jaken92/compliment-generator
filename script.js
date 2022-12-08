7bd46084ad7617f23facba0c7d4d9398e1d6adc7
jaken92 <jaaakeeen@gmail.com>
2022-12-08 22:45:32 +0100

    added javascript for scrollevent

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const listItems = document.querySelectorAll('ul li');
 const list = document.querySelector('ul');
 const body = document.querySelector('body');
+const selected = document.querySelector('.selected');
 
 const compliments = [
   'Nice',
@@ -16,12 +17,80 @@ const compliments = [
   'Empathic',
   'Sexy',
   'Fabulous',
-  'Twinkly',
+  'Awesome',
   'Shiny',
-  'Superstar',
+  'Fantastic',
   'Cool',
   'Down with the kids',
 ];
+
+const preliments = [
+  'Absurdly',
+  'Amazingly',
+  'Profoundly',
+  'Abnormally',
+  'Beautifully',
+  'Clearly',
+  'Majestically',
+  'Unbelievably',
+  'Neatly',
+  'Properly',
+  'Rediculously',
+  'Truthfully',
+  'Viciously',
+  'Incredibly',
+];
+
+body.addEventListener('click', function () {
+  listItems.forEach(function (listItem, i) {
+    listItem.textContent =
+      preliments[Math.floor(Math.random() * preliments.length)] +
+      ` ${compliments[Math.floor(Math.random() * compliments.length)]}`;
+  });
+});
+
+const windowHeight = window.innerHeight;
+// window.addEventListener('scroll', function () {
+//   const scroll = window.scrollY;
+//   if (scroll > tester.offsetTop - windowHeight * 0.5) {
+//     body.classList.add('blue');
+//   } else {
+//     body.classList.remove('blue');
+//   }
+// });
+
+window.addEventListener('scroll', function () {
+  const scroll = window.scrollY;
+  listItems.forEach(function (listItem, i) {
+    if (scroll > listItem.offsetTop - windowHeight * 0.55) {
+      for (i = 0; i < list.children.length; i++) {
+        list.children[i].classList.remove('selected');
+      }
+      listItem.classList.add('selected');
+      body.style.background = 'blue';
+    }
+  });
+});
+
+// selected.addEventListener('scroll', function () {
+//   const scrollposs = selected.scrollTop;
+//   console.log(scrollposs);
+// });
+
+// window.addEventListener('scroll', function () {
+//   console.log(window.pageYOffset);
+// });
+
+// window.onscroll = function() {myFunction()};
+
+// function myFunction() {
+//   if (document.documentElement.scrollTop > window.height / 2) {
+//     document.getElementById("").className = "test";
+//   } else {
+//     document.getElementById("myP").className = "";
+//   }
+// }
+/*working ex
 
 body.addEventListener('click', function () {
   listItems.forEach(function (listItem, i) {
@@ -32,6 +101,7 @@ body.addEventListener('click', function () {
     compliments[Math.floor(Math.random() * compliments.length)]
   }`;
 });
+*/
 
 /*
 element.addEventListener("click", function() {
@@ -42,6 +112,24 @@ element.addEventListener("click", function() {
 
 
 
+
+body.addEventListener('scroll', function () {
+  const scrollTop = body.scrollTop;
+  listItems.forEach(function (listItem, i) {
+    if (listItem.offsetTop < scrollTop + body.offsetHeight / 2) {
+      list.children[i].classList.add('selected');
+    }
+  });
+});
+
+
+/*
+//inspo: https://www.youtube.com/watch?v=FDCiM6JoPao&t=47s
+// https://stackoverflow.com/questions/2481350/how-can-i-get-the-scrollbar-position-with-javascript
+
+const items = document.querySelectorAll('.items');
+const scrollMenu = document.querySelector('.scrollmenu');
+const indicators = document.querySelector('.indicators');
 
 //removing the class .selected from all li elements when called.
 function removeSelected() {
@@ -67,4 +155,4 @@ scrollMenu.addEventListener('scroll', function () {
     }
   });
 });
-  */
+*/
